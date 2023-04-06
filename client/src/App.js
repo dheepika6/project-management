b4f422cb122ab9620dc1db0ce6e768d7a55b6568
Dheepika <rdheepikaamrita@gmail.com>
2023-04-06 02:48:04 -0600

    adding route and pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
-import Header from "./components/Header";
-import Clients from "./components/Clients";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
-import AddClientModal from "./components/AddClientModal";
-import Projects from "./components/Projects";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Project from "./pages/Project";
+import Header from "./components/Header";
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -31,13 +32,16 @@ function App() {
   return (
     <>
       <ApolloProvider client={client}>
-        <Header />
-        <div className="container">
-          <AddClientModal />
-          <Projects />
-          <Clients />
-          Hello
-        </div>
+        <Router>
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/project/:id" element={<Project />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
+        </Router>
       </ApolloProvider>
     </>
   );
